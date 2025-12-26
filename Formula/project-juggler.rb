bc82836cbd70750a11ca0c70c3f901bbf6a8cb8e
Max Medvedev <maxim.medvedev@jetbrains.com>
2025-12-27 00:17:35 +0100

    rebrand to project-juggler, version 0.0.1

--- Formula/project-juggler.rb
+++ Formula/project-juggler.rb
@@ -1,7 +1,7 @@
-class IdeaJuggler < Formula
+class ProjectJuggler < Formula
   desc "CLI tool that manages separate IntelliJ IDEA instances per project"
-  homepage "https://github.com/maxmedvedev/idea-juggler"
-  url "https://github.com/maxmedvedev/idea-juggler/releases/download/0.0.4/idea-juggler-0.0.4.tar.gz"
+  homepage "https://github.com/maxmedvedev/project-juggler"
+  url "https://github.com/maxmedvedev/project-juggler/releases/download/0.0.1/project-juggler-0.0.1.tar.gz"
   sha256 "55e56277f55475fe142fe364393d3345531be40e570707188d9805b3ef8aa8bb"
   license "MIT"
 
@@ -12,7 +12,7 @@ class IdeaJuggler < Formula
     libexec.install Dir["libexec/*"]
 
     # Install pre-built wrapper script
-    bin.install "bin/idea-juggler"
+    bin.install "bin/project-juggler"
 
     # Ensure JAVA_HOME points to Homebrew's OpenJDK
     # The wrapper script will use this automatically
@@ -21,7 +21,7 @@ class IdeaJuggler < Formula
 
   def caveats
     <<~EOS
-      idea-juggler requires Java 17+.
+      project-juggler requires Java 17+.
 
       If you encounter Java-related issues, set JAVA_HOME explicitly:
         export JAVA_HOME="#{Formula["openjdk@17"].opt_prefix}/libexec/openjdk.jdk/Contents/Home"
@@ -29,8 +29,8 @@ class IdeaJuggler < Formula
   end
 
   test do
-    output = shell_output("#{bin}/idea-juggler --help 2>&1")
-    assert_match "idea-juggler", output.downcase
+    output = shell_output("#{bin}/project-juggler --help 2>&1")
+    assert_match "project-juggler", output.downcase
     assert_match "version", output.downcase
   end
 end
